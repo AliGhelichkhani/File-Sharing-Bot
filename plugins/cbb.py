5312cace943e35527a74e9d4bf4d3489c3f58487
Ali Ghelichkhani <awwligh@gmail.com>
2024-11-18 21:54:46 +0330

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = "یادگیری نامحدود تکنولوژی به صورت رایگان🧠 \n خوش اومدی به مموری لیک شو🙋🏻‍♂️ \n ویدیوهای آموزش برنامه نویسی و مباحث مرتبط هفتگی توی یوتوب آپلود میشه \n یادت نره سابسکرایب کنی👇\n https://www.youtube.com/@memoryleaksho?sub_confirmation=1",
+            text = "📌 آدرس شبکه های اجتماعی:\r\n\r\nایمیل 📩 : \r\nmemoryleaksho@gmail.com\r\n\r\nاینستاگرام🔎:\r\nhttps://instagram.com/memoryleaksho\r\n\r\nیوتوب▶️:\r\nhttps://www.youtube.com/@memoryleaksho?sub_confirmation=1\r\n\r\nگیت‌هاب👾:\r\nhttps://github.com/MemoryLeakSho\r\n\r\nحمایت مالی🎁:\r\nhttps://zarinp.al/memoryleaksho\r\n\r\nپشتیبانی💬:\r\n@MemoryLeakShoContact \r\n\r\nگروه تلگرام 👥:\r\nt.me/MemoryLeakShoDiscussion\r\n\r\nگروه تخصصی🧑🏻‍💻:\r\nt.me/MemoryLeakShoTopics\r\n\r\nچت ناشناس👤:\r\nhttps://t.me/HidenChat_Bot?start=115463811\r\n\r\n\r\nمموری لیک شو | یادگیری نامحدود تکنولوژی به صورت رایگان. 🧠",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
